a77a9a6ad29bddf0d7c0d14a9a94f2724c87ba8f
Edmundo Schultz Neto <edmundo.neto@4all.com>
2017-07-01 09:16:54 -0300

    prepared

---
+++ app/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import Navigation from './navigation';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Navigation />
+        )
+    }
+}
---
+++ app/navigation/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+
+
+const AppNavigatior = StackNavigator({
+
+})
+
+export default AppNavigatior;
